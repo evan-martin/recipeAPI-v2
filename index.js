ae13df3910aaecf7287f2e0afb4d653f05cec405
evan-martin <evanm689@gmail.com>
2022-08-15 17:08:58 -0700

    consolidate environmental variable config files

--- index.js
+++ index.js
@@ -4,49 +4,34 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const { expressjwt: jwt } = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
-const authConfig = require("./auth_config.json");
 const Recipe = require('./recipe.schema.js')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const bp = require('body-parser')
 
-const port = process.env.API_PORT || 3001;
-const appPort = process.env.SERVER_PORT || 3000;
-const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(cors())
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors({ origin: appOrigin }));
+app.use(cors({ origin: process.env.APPORIGIN }));
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 dotenv.config({ path: './.env' });
-
-if (
-  !authConfig.domain ||
-  !authConfig.audience ||
-  authConfig.audience === "YOUR_API_IDENTIFIER"
-) {
-  console.log(
-    "Exiting: Please make sure that auth_config.json is in place and populated with valid domain and audience values"
-  );
-
-  process.exit();
-}
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+    jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`,
   }),
 
-  audience: authConfig.audience,
-  issuer: `https://${authConfig.domain}/`,
+  audience: process.env.AUDIENCE,
+  issuer: `https://${process.env.DOMAIN}/`,
   algorithms: ["RS256"],
 });
 
