c4fe75b0e665a3e0eb997c57f09aebd592443b20
evan-martin <evanm689@gmail.com>
2022-08-22 16:19:24 -0700

    add shopping list field to new user route

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ app.post("/api/recipes/new-user", checkJwt, (req, response) => {
   Recipe.create({
     email: req.body.email,
     recipes: defaultRecipes,
+    shoppingList: [],
   }, (err, result) => {
     if (err) console.log(err)
     response.send(result)
@@ -97,6 +98,7 @@ app.post("/api/recipes/new-user", checkJwt, (req, response) => {
 
 //update shopping list:
 app.put("/api/recipes/update-shopping-list", checkJwt, (req, response) => {
+  console.log(req.body.shoppingList)
   Recipe.findOneAndUpdate({ email: req.headers.user },
     {
       shoppingList: req.body.shoppingList
