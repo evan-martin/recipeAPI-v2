e54e23c199c8c3a6f2962fbc328e7bf5c50df809
evan-martin <evanm689@gmail.com>
2022-08-23 18:11:01 -0700

    add cors header for local development

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001
 const db = (process.env.DB)
 const domain = (process.env.DOMAIN)
 const audience = (process.env.AUDIENCE)
-const appOrigin = (process.env.APPORIGIN)
+const appOrigin = (process.env.APPORIGIN || process.env.LOCAL_HOST)
 
 app.use(cors())
 app.use(morgan("dev"))
